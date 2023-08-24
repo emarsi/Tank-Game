bcb9de124e95ee98efe65ef6f46d66236ad26a00
Eric Marsi <emarsi@uwo.ca>
2023-08-23 22:56:14 -0400

    Added Second Player
    - player 1 controls with wasd
    - player 2 controls with ijkl
    - added turn system
    - added UI indicator of whose turn it is
    - imported TextMeshPro

--- Tank Game/Assets/Scripts/Tank.cs
+++ Tank Game/Assets/Scripts/Tank.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Tank : MonoBehaviour
 {
 
-    public Transform firePoint;
+    public Transform firePoint1;
+    public Transform firePoint2;
 
     public GameObject bullet;
 
-    public Transform gun;
+    public Transform gun1;
+    public Transform gun2;
 
-    public Rigidbody2D rb;
     public Transform tank1;
+    public Transform tank2;
 
     public float rotateSpeed = 1f;
     public float moveSpeed = 5f;
@@ -21,42 +24,87 @@ public class Tank : MonoBehaviour
 
     float direction = 0;
 
+    //track player turn
+    bool playerOneTurn = true;
+
+    public TMP_Text turnText;
+
     // Update is called once per frame
     void Update()
     {
+        //player 1 controls
+        if (playerOneTurn)
+        {
+            //player 1 turret up/down movement
+            if (Input.GetKey("w")) //up
+            {
+                if (gun1.transform.rotation.z < 1)
+                {
+                    gun1.transform.Rotate(0, 0, rotateSpeed, Space.Self);
+                }
+            }
+            if (Input.GetKey("s")) //down
+            {
+                if (gun1.transform.rotation.z > 0)
+                {
+                    gun1.transform.Rotate(0, 0, -1 * rotateSpeed, Space.Self);
+                }
+            }
+
+            //player 1 left and right movement
+            if (Input.GetKey("d")) //right
+            {
+                direction = 1;
+            }
+            else if (Input.GetKey("a")) //left
+            {
+                direction = -1;
+            }
+            else //no input
+            {
+                direction = 0;
+            }
+        }
+        else if (!playerOneTurn)
+        {
+            //player 2 turret up/down movement
+            if (Input.GetKey("k")) //down
+            {
+                if (gun2.transform.rotation.z < 1)
+                {
+                    gun2.transform.Rotate(0, 0, rotateSpeed, Space.Self);
+                }
+            }
+            if (Input.GetKey("i")) //up
+            {
+                if (gun2.transform.rotation.z > 0)
+                {
+                    gun2.transform.Rotate(0, 0, -1 * rotateSpeed, Space.Self);
+                }
+            }
+
+            //player 1 left and right movement
+            if (Input.GetKey("l")) //right
+            {
+                direction = 1;
+            }
+            else if (Input.GetKey("j")) //left
+            {
+                direction = -1;
+            }
+            else //no input
+            {
+                direction = 0;
+            }
+        }
+
+        //shoot 
         if (Input.GetKeyDown("space"))
         {
             Shoot();
-        }
-
-        //player 1 turret up/down movement
-        if (Input.GetKey("w"))
-        {
-            if (gun.transform.rotation.z < 1)
-            {
-                gun.transform.Rotate(0, 0, rotateSpeed, Space.Self);
-            }
-        }
-        if (Input.GetKey("s"))
-        {
-            if (gun.transform.rotation.z > 0)
-            {
-                gun.transform.Rotate(0, 0, -1 * rotateSpeed, Space.Self);
-            }
-        }
-
-        //player 1 left and right movement
-        if (Input.GetKey("d"))
-        {
-            direction = 1;
-        }
-        else if (Input.GetKey("a"))
-        {
-            direction = -1;
-        }
-        else
-        {
-            direction = 0;
+            //cycle turn
+            playerOneTurn = !playerOneTurn;
+            UpdateTurn();
         }
     }
 
@@ -64,12 +112,38 @@ public class Tank : MonoBehaviour
     {
         //moves tank left and right
         moveDir = new Vector2(direction, 0);
-        tank1.Translate(moveDir * moveSpeed * Time.fixedDeltaTime);
+        if (playerOneTurn)
+        {
+            tank1.Translate(moveDir * moveSpeed * Time.fixedDeltaTime);
+        }
+        else if (!playerOneTurn)
+        {
+            tank2.Translate(moveDir * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 
     void Shoot()
     {
         //create and shoot bullet
-        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        if (playerOneTurn)
+        {
+            Instantiate(bullet, firePoint1.position, firePoint1.rotation);
+        }
+        else if (!playerOneTurn)
+        {
+            Instantiate(bullet, firePoint2.position, firePoint2.rotation);
+        }
+    }
+
+    void UpdateTurn()
+    {
+        if (playerOneTurn)
+        {
+            turnText.text = "Player 1 Turn";
+        }
+        else if (!playerOneTurn)
+        {
+            turnText.text = "Player 2 Turn";
+        }
     }
 }
